9547df183c9a6aec3f058f76dc729539a3c24a25
fanxy <fanxiaoyi1995@126.com>
2023-03-23 17:58:54 +0800

    feat: deposit/claim ETH to recipients

--- contracts/TokenDistribution.sol
+++ contracts/TokenDistribution.sol
@@ -3,16 +3,30 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract TokenDistribution {
+
+    uint8 constant RECORD_TYPE_RECIPIENTS = 1;
+    uint8 constant RECORD_TYPE_ROOM = 2;
+
     address public owner;
     uint256 public nextDepositId;
     mapping(uint256 => Record) public records;
+    mapping(uint256 => mapping(address => ClaimInfo)) public claimInfos;
 
     struct Record {
         address sender;
+        address tokenAddress;
+        uint8 recordType; // 1-To recipients 2-To room
+        address[] recipients; // for type 1
+        uint32 roomId; // for type 2
         uint256 amount;
-        uint256 totalCount;
-        uint256 remainCount;
+        uint32 totalCount;
+        uint32 remainCount;
         uint256 expiredTime;
+    }
+
+    struct ClaimInfo {
+        address recipient;
+        uint256 claimTime;
     }
 
     constructor() {
@@ -28,10 +42,25 @@ contract TokenDistribution {
         owner = newOwner;
     }
 
-    function deposit(uint256 totalCount, uint256 expiredTime) public payable {
+    function depositETHToRecipients(uint32 totalCount, address[] memory recipients, uint256 expiredTime) public payable {
         require(msg.value > 0, "Deposit amount is zero.");
-        records[nextDepositId] = Record(msg.sender, msg.value, totalCount, totalCount, expiredTime);
+        require(totalCount > 0, "Total count must be greater than zero.");
+        require(recipients.length >= totalCount, "The number of recipients must be greater than or equal to the total count.");
+        records[nextDepositId] = Record(msg.sender, address(0), RECORD_TYPE_RECIPIENTS, recipients, 0, msg.value, totalCount, totalCount, expiredTime);
         nextDepositId++;
+    }
+
+    function depositETHToRoom(uint32 totalCount, uint32 roomId, uint256 expiredTime) public payable {
+        require(msg.value > 0, "Deposit amount is zero.");
+        require(totalCount > 0, "Total count must be greater than zero.");
+        records[nextDepositId] = Record(msg.sender, address(0), RECORD_TYPE_ROOM, new address[](0), roomId, msg.value, totalCount, totalCount, expiredTime);
+        nextDepositId++;
+    }
+
+    function depositErc20ToRecipients(uint32 totalCount, address[] memory recipients, uint256 expiredTime, address tokenAddress) public {
+    }
+
+    function depositErc20ToRoom(uint32 totalCount, uint32 roomId, uint256 expiredTime, address tokenAddress) public {
     }
 
     function claim(uint256 depositId, address recipient) public onlyOwner {
@@ -39,19 +68,52 @@ contract TokenDistribution {
         require(records[depositId].sender != address(0), "Invalid deposit ID.");
         require(records[depositId].expiredTime >= block.timestamp, "Deposit expired.");
         require(records[depositId].remainCount > 0, "Invalid deposit remainCount.");
-        uint256 amount = records[depositId].amount / records[depositId].totalCount;
-        records[depositId].remainCount--;
-        payable(recipient).transfer(amount);
+        require(claimInfos[depositId][recipient].recipient == address(0), "Already claimed.");
+        if (records[depositId].recordType == RECORD_TYPE_RECIPIENTS) {
+            require(isInArray(recipient, records[depositId].recipients), "Invalid recipient.");
+            if (records[depositId].tokenAddress == address(0)) {
+                uint256 amount = records[depositId].amount / records[depositId].totalCount;
+                records[depositId].remainCount--;
+                claimInfos[depositId][recipient].recipient = recipient;
+                claimInfos[depositId][recipient].claimTime = block.timestamp;
+                payable(recipient).transfer(amount);
+            } else {
+                // todo
+            }
+        } else if (records[depositId].recordType == RECORD_TYPE_ROOM) {
+            if (records[depositId].tokenAddress == address(0)) {
+                uint256 amount = records[depositId].amount / records[depositId].totalCount;
+                records[depositId].remainCount--;
+                claimInfos[depositId][recipient].recipient = recipient;
+                claimInfos[depositId][recipient].claimTime = block.timestamp;
+                payable(recipient).transfer(amount);
+            } else {
+                // todo
+            }
+        }
     }
 
-    function claimToSender(uint256 depositId) public onlyOwner {
+    function claimToSender(uint256 depositId) public {
         require(records[depositId].sender != address(0), "Invalid deposit ID.");
         require(msg.sender == records[depositId].sender, "Only the sender can claim.");
         require(records[depositId].expiredTime < block.timestamp, "Only expired deposit can claim to sender.");
         require(records[depositId].remainCount > 0, "Invalid deposit remainCount.");
         uint256 amount = records[depositId].amount / records[depositId].totalCount * records[depositId].remainCount;
-        records[depositId].remainCount = 0;
-        payable(msg.sender).transfer(amount);
+        if (records[depositId].tokenAddress == address(0)) {
+            records[depositId].remainCount = 0;
+            payable(msg.sender).transfer(amount);
+        } else {
+            // todo
+        }
+    }
+
+    function isInArray(address addr, address[] memory array) private pure returns(bool) {
+        for(uint i=  0; i < array.length; i++) {
+            if(array[i] == addr) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -6,31 +6,39 @@ const { time } = require("@nomicfoundation/hardhat-network-helpers");
 const TEST_ADDRESS = "0x726063423641b0d028D17E32960A0288B818783d"
 
 describe("TokenDistribution", async function () {
-  const TokenDistribution = await ethers.getContractFactory("TokenDistribution");
-  const tokenDistribution = await TokenDistribution.deploy();
-  await tokenDistribution.deployed();
-  it("Deposit and claim", async function () {
 
+  it("DepositETHToRecipients and claim", async () => {
+    const tokenDistribution = await deployContract();
     const recipient = await ethers.getSigner(TEST_ADDRESS);
-    await tokenDistribution.deposit(1, tomorrow(), {value: "100"});
+    await tokenDistribution.depositETHToRecipients(1, [TEST_ADDRESS], tomorrow(), {value: "100"});
     await tokenDistribution.claim(0, TEST_ADDRESS);
     const balance1 = await recipient.getBalance();
     expect(balance1.toString()).eq("100")
 
-    await tokenDistribution.deposit(2, tomorrow(), {value: "100"});
+    await tokenDistribution.depositETHToRecipients(2, [TEST_ADDRESS, TEST_ADDRESS], tomorrow(), {value: "100"});
     await tokenDistribution.claim(1, TEST_ADDRESS);
     const balance2 = await recipient.getBalance();
     expect(balance2.toString()).eq("150")
 
   });
 
-  it("Claim to sender", async function () {
+  it("DepositETHToRoom and claim", async () => {
+    const tokenDistribution = await deployContract();
+    const recipient = await ethers.getSigner(TEST_ADDRESS);
+    await tokenDistribution.depositETHToRoom(1, 1, tomorrow(), {value: "100"});
+    await tokenDistribution.claim(0, TEST_ADDRESS);
+    const balance1 = await recipient.getBalance();
+    expect(balance1.toString()).eq("250")
+
+    await tokenDistribution.depositETHToRoom(2, 1, tomorrow(), {value: "100"});
+    await tokenDistribution.claim(1, TEST_ADDRESS);
+    const balance2 = await recipient.getBalance();
+    expect(balance2.toString()).eq("300")
 
     expect(await tokenDistribution.provider.getBalance(tokenDistribution.address)).eq("50")
-    await time.setNextBlockTimestamp(tomorrow());
+    await time.setNextBlockTimestamp(tomorrow() + 3600);
     await tokenDistribution.claimToSender(1);
     expect(await tokenDistribution.provider.getBalance(tokenDistribution.address)).eq("0")
-
   });
 });
 
@@ -40,4 +48,11 @@ function now() {
 
 function tomorrow() {
   return now() + 60 * 60 * 24
+}
+
+async function deployContract() {
+  const TokenDistribution = await ethers.getContractFactory("TokenDistribution");
+  const tokenDistribution = await TokenDistribution.deploy();
+  await tokenDistribution.deployed();
+  return tokenDistribution
 }
